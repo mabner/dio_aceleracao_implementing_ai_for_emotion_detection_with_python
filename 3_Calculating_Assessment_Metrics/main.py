2785f135e374c66fdea5caf95a7102d40b1f8edd
Marcos Leite <m.abner@live.co.uk>
2024-02-15 04:11:02 -0300

    3rd challenge completed

--- 3_Calculating_Assessment_Metrics/main.py
+++ 3_Calculating_Assessment_Metrics/main.py
@@ -13,6 +13,7 @@ def best_performance(matrices):
     best_index = 0
     best_accuracy = 0
     best_precision = 0
+    best_metrics = 0
     # TODO: Define Loop through each matrix to calculate metrics
     for index, matrix in enumerate(matrices):
         # TODO: Define tp, fp fn and tn
@@ -27,24 +28,20 @@ def best_performance(matrices):
         precision = tp / (tp + fp)
 
         # TODO: Update best metrics if found
-        print(f"Index:{index+1}, Accuracy: {accuracy:.2f}, Precision: {precision:.2f}, "
-              f"Combined: {(accuracy + precision):.2f}")
+        combined_metrics = [accuracy, precision]
+        sum_metrics = [index, sum(combined_metrics)]
+        if sum_metrics[1] > best_metrics:
+            best_metrics = sum_metrics[1]
+            best_index = index
+            best_accuracy = accuracy
+            best_precision = precision
+        else:
+            continue
 
-    return best_index, best_accuracy, best_precision
+    return (f"""Index: {best_index + 1} 
+Accuracy: {round(best_accuracy, 2)}
+Precision: {round(best_precision, 2)}""")
 
 
 # Print the results
-best_performance(matrices)
-print("-----------")
-"""Sample data:
-4
-70,15,8,78
-60,20,10,80
-45,5,3,92
-80,7,15,98
-
-Return:
-Index: 3
-Accuracy: 0.94
-Precision: 0.9
-"""
+print(best_performance(matrices))
