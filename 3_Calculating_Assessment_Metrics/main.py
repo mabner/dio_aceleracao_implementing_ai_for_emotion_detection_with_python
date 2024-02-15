199b6264463fae3f74186a6c0dfdf8cd894cea1c
Marcos Leite <m.abner@live.co.uk>
2024-02-15 02:05:51 -0300

    adds sample input and output as comment

--- 3_Calculating_Assessment_Metrics/main.py
+++ 3_Calculating_Assessment_Metrics/main.py
@@ -15,12 +15,27 @@ def best_performance(matrices):
     best_precision = 0
     # TODO: Define Loop through each matrix to calculate metrics
     for index, matrix in enumerate(matrices):
-    # TODO: Define tp, fp fn and tn
+        # TODO: Define tp, fp fn and tn
 
-    # TODO: Calculate accuracy and precision
+        # TODO: Calculate accuracy and precision
 
-    # TODO: Update best metrics if found
+        # TODO: Update best metrics if found
 
     return best_index, best_accuracy, best_precision
 
+
 # Print the results
+best_performance(matrices)
+
+"""Sample data:
+4
+70,15,8,78
+60,20,10,80
+45,5,3,92
+80,7,15,98
+
+Return:
+Index: 1
+Accuracy: 1.0
+Precision: 1.0
+"""
