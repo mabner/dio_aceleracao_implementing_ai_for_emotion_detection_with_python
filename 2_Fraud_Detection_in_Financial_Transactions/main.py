3f4d7c9c95ce8a74a115a3f1547dd7d098fa1c91
Marcos Leite <m.abner@live.co.uk>
2024-02-15 00:09:15 -0300

    transaction list split done

--- 2_Fraud_Detection_in_Financial_Transactions/main.py
+++ 2_Fraud_Detection_in_Financial_Transactions/main.py
@@ -5,17 +5,21 @@ for n in range(0, n):
     lines.append(input())
 
 # TODO: Split each line into individual transactions and append them to a transaction list
+transactions = [line.split(", ") for line in lines]
 
 # TODO: Define a function to detect fraudulent transactions
 def detect_frauds(transactions):
     suspicious_transactions = []
 
    # TODO: Iterate over each transaction
-    for transaction in transactions:
-        # TODO: Unpack the transaction into its components: id, value, and threshold and convert transaction value and fraud threshold to integers
+    #for transaction in transactions:
 
-        # TODO: Check if the transaction value exceeds the fraud threshold
+        # TODO: Unpack the transaction into its components: id, value, and threshold and convert transaction value
+        #  and fraud threshold to integers
+
+    # TODO: Check if the transaction value exceeds the fraud threshold
 
     return '\n'.join(suspicious_transactions)
 
 # TODO: Print the suspicious transactions list
+print(transactions)
